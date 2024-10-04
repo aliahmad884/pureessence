e16f14e2c567fa844af9ccf5b76e86c86166e445
Obaid Naseer <114626331+obaidnaseer@users.noreply.github.com>
2024-10-04 06:04:33 +0500

    test about p

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,16 +2,25 @@ export default function AboutPage(){
     return(
         <>
         <h1>About Pure Essence</h1>
-        <p>Pure Essence LTD intro paragraph</p>
+        <p>Pure Essence LTD  paragraph</p>
+
         <h2>Our Story</h2>
+        <p>Pure Essence LTD  paragraph</p>
 
         <h2>Our Mission</h2>
+        <p>Pure Essence LTD  paragraph</p>
 
         <h2>Our Vision</h2>
+        <p>Pure Essence LTD  paragraph</p>
+
 
         <h2>Why Choose Pure Essence</h2>
+        <p>Pure Essence LTD  paragraph</p>
+
 
         <h2>Meet Our Team</h2>
+        <p>Pure Essence LTD  paragraph</p>
+
 
         <h2>Connect With Us</h2>
         <p>Pure Essence Social and Contact Page Link</p>
