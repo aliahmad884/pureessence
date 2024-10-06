e5f494392a9470d11fd4b5761b26f40de935117d
aliahmad884 <alilatakhun2003@gmail.com>
2024-10-06 21:49:02 +0500

    About & FAQs Completed

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,30 +1,62 @@
-export default function AboutPage(){
-    return(
+export default function AboutPage() {
+    return (
         <>
-        <h1>About Pure Essence</h1>
-        <p>Pure Essence LTD  paragraph</p>
+        <div className="aboutBanner">
+            <h1>About Us</h1>
+        </div>
+            <div className="aboutPage">
+                <div className="firstCont">
+                    <div className="postCont">
+                        <div className="text">
+                            <h2>Our Mission</h2>
+                            <p>Our aim is to provide tech solutions to empower businesses and everyday uses both with proprietary and open-source code.</p>
+                            <h2>Our Vision</h2>
+                            <p><b>&quot;The best things in life are free&quot;</b> At AldorTech, we not only help businesses by providing tech solutions, but mentor businesses to use open source and cost effective tools. Tools that help businesses grow without an anchor weighing them down in the form of expensive subscriptions and poorly written software.
+                            </p>
+                        </div>
+                        <div className="img">
+                            <img src="/PE-Honey-P3.webp" alt="Support" />
+                        </div>
+                    </div>
+                    <div className="subCont">
+                        <h3>Sub headung</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa commodi maiores aliquid atque minus, accusamus dolor illum aspernatur veritatis. Et minus ullam natus ex sed nemo fuga reiciendis esse voluptas?</p>
+                        <h3>Sub Heading</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem quos quia inventore molestias nobis vero sed temporibus, officiis libero cumque, deserunt ea laborum maxime quo repudiandae dignissimos. Molestias, aliquid quis.</p>
+                    </div>
+                </div>
 
-        <h2>Our Story</h2>
-        <p>Pure Essence LTD  paragraph</p>
-
-        <h2>Our Mission</h2>
-        <p>Pure Essence LTD  paragraph</p>
-
-        <h2>Our Vision</h2>
-        <p>Pure Essence LTD  paragraph</p>
-
-
-        <h2>Why Choose Pure Essence</h2>
-        <p>Pure Essence LTD  paragraph</p>
-
-
-        <h2>Meet Our Team</h2>
-        <p>Pure Essence LTD  paragraph</p>
-
-
-        <h2>Connect With Us</h2>
-        <p>Pure Essence Social and Contact Page Link</p>
-
+                <div className="secondCont globalBG">
+                    <h3>DISTINCTIONS THAT DEFINE US</h3>
+                    <ul>
+                        <li>Distributed Approach</li>
+                        <li>Standardized Excellence</li>
+                        <li>Privacy Pioneers</li>
+                        <li>Auditability Architects</li>
+                        <li>Security Stalwarts</li>
+                        <li>Global Client Satisfaction</li>
+                        <li>Adept IT Professionals</li>
+                        <li>Unwavering Quality</li>
+                        <li>24/7 Customer Support</li>
+                        <li>Punctual Deliveries</li>
+                        <li>Agile Methodology Advocates</li>
+                        <li>Customer Devotion</li>
+                        <li>Adaptive Flexibility</li>
+                        <li>Catalysts for Business Growth</li>
+                    </ul>
+                    <div className="postCont">
+                        <div className="text">
+                            <h2>DIGITAL EVOLUTION WITH ALDOR TECHNOLOGIES</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quisquam aperiam qui tempore minima corrupti corporis possimus delectus temporibus laudantium tenetur ex, nostrum ipsam deserunt aliquam aspernatur eligendi harum earum?</p>
+                            <h2>Heading Text!</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum esse, ipsam voluptas obcaecati impedit soluta vel eos tenetur alias dicta amet nesciunt suscipit possimus, maiores quisquam! Vero earum maxime ea!</p>
+                        </div>
+                        <div className="img">
+                            <img src="/PE-Honey-B1.webp" alt="Honey" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- app/frequently-asked-questions/page.jsx
+++ app/frequently-asked-questions/page.jsx
@@ -1,7 +1,55 @@
+"use client"
+import { faGreaterThan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Accordion, Placeholder } from "rsuite";
+import { useEffect, useState } from 'react'
+import FallBackLoader from "@/components/loader";
+
+
 export default function FAQs() {
+    const [isLoading, setIsloading] = useState(true)
+    useEffect(() => {
+        let headers = document.querySelectorAll('.rs-panel-btn')
+        let headerCont = document.querySelectorAll('.rs-panel-header')
+        headers.forEach((btn, i) => {
+            headerCont[i].style.padding = '0'
+            btn.style.backgroundColor = '#dfb434'
+            btn.style.padding = '20px 20px'
+            // btn.style.borderBottomLeftRadius = '0'
+            // btn.style.borderBottomRightRadius = '0'
+            // btn.style.marginBottom = '15px'
+        })
+        // setIsloading(false)
+    }, [])
+    // if (isLoading) return <FallBackLoader />
     return (
         <>
-            <h1>frequently-asked-questions</h1>
+            <div className="faqsCont">
+                <div className="accordCont">
+                    <h1>FAQs</h1>
+                    <Accordion defaultActiveKey={1} style={{ padding: '0 10px' }}>
+                        <Accordion.Panel className="accordPanel" classPrefix="temp" eventKey={1} header='Panel 1'>
+                            <p style={{ textAlign: 'justify', paddingTop: '20px' }}>orem ipsum dolor, sit amet consectetur adipisicing elit. Tempore sed, expedita earum et dolorem modi suscipit laboriosam neque quidem enim praesentium velit fugit impedit laborum amet dolorum aspernatur quia? Cum.
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo dolorum recusandae impedit corporis obcaecati exercitationem ad esse nisi provident laborum ipsam autem, magni beatae tempore. Nisi corporis itaque doloribus perferendis.
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus velit assumenda, aliquid vel odit iusto voluptates at! Assumenda neque vitae, earum aspernatur tenetur rerum molestiae animi, maiores voluptatem quibusdam itaque?</p>
+                        </Accordion.Panel>
+                        <Accordion.Panel className="accordPanel" classPrefix="temp" header='Panel 1' eventKey={2}>
+                            <p style={{ textAlign: 'justify', paddingTop: '20px' }}>orem ipsum dolor, sit amet consectetur adipisicing elit. Tempore sed, expedita earum et dolorem modi suscipit laboriosam neque quidem enim praesentium velit fugit impedit laborum amet dolorum aspernatur quia? Cum.
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo dolorum recusandae impedit corporis obcaecati exercitationem ad esse nisi provident laborum ipsam autem, magni beatae tempore. Nisi corporis itaque doloribus perferendis.
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus velit assumenda, aliquid vel odit iusto voluptates at! Assumenda neque vitae, earum aspernatur tenetur rerum molestiae animi, maiores voluptatem quibusdam itaque?</p>
+                        </Accordion.Panel>
+                        <Accordion.Panel className="accordPanel" classPrefix="temp" header='Panel 1' eventKey={3}>
+                            <p style={{ textAlign: 'justify', paddingTop: '20px' }}>orem ipsum dolor, sit amet consectetur adipisicing elit. Tempore sed, expedita earum et dolorem modi suscipit laboriosam neque quidem enim praesentium velit fugit impedit laborum amet dolorum aspernatur quia? Cum.
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo dolorum recusandae impedit corporis obcaecati exercitationem ad esse nisi provident laborum ipsam autem, magni beatae tempore. Nisi corporis itaque doloribus perferendis.
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus velit assumenda, aliquid vel odit iusto voluptates at! Assumenda neque vitae, earum aspernatur tenetur rerum molestiae animi, maiores voluptatem quibusdam itaque?</p>
+                        </Accordion.Panel>
+                    </Accordion>
+                    <div className='infoCont'>
+                        <h4>For more info lorem</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat dignissimos ea distinctio, dolores maiores rerum eum praesentium, facere aliquam cum eaque at et sapiente. Quam alias recusandae quis sequi rerum.lor Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae minus neque fugiat pariatur aperiam consequatur cum distinctio nostrum animi, corporis, labore ullam earum modi nam corrupti obcaecati quo quibusdam nihil.</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
