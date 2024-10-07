269233e2c25fbb62a2ebd5df752a71f0479d2772
Obaid Naseer <114626331+obaidnaseer@users.noreply.github.com>
2024-10-07 23:12:50 +0500

    Products Data, Ready for Live!

--- app/blogs/health-guides/page.jsx
+++ app/blogs/health-guides/page.jsx
@@ -4,7 +4,7 @@ export default function GuidesPage() {
             <br /><br />
             <div className="blogHeading">
                 <h1>Guides Page</h1>
-                <br /><hr />
+                <br /><hr /><br />
                 <h2>Our Health Guides Are Coming Soon.</h2>
             </div>
             <br /><br />
--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -4,7 +4,7 @@ export default function BlogsPage() {
             <br /><br />
             <div className="blogHeading">
                 <h1>Blogs Page</h1>
-                <br /><hr />
+                <br /><hr /><br />
                 <h2>Our Blogs Are Coming Soon.</h2>
             </div>
             <br /><br />
