cb2e37b4b1212ea68b11a2cf4a4a0a1e08a6b989
Ali Ahmad <143416088+aliahmad884@users.noreply.github.com>
2024-10-07 17:25:59 +0500

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -15,7 +15,7 @@ export default function AboutPage() {
                             Puressence is not only for recovering from ailment, but to preserve and better health. To do that we ensure to only use the most natural products. We only serve what excites us- natural health and products- and we believe our customers will feel the same way. Knowing that the sources are pure give us satisfaction to use these products ourselves, and to serve our customers.
                             </p>
                             <h2>Our Mission</h2>
-                            <p>Our mission at PurEssence is to provide everyday people with the best quality product. The same quality that is found in nature.<br />We further commit to inform and motivate user to live a healthy lifestyle by being in sync with nature. The biggest way to do that is our biggest physical input, which is our mouths. By serving the best health foods and health supplements, we wish to boost our customers' health and lives.</p>
+                            <p>Our mission at PurEssence is to provide everyday people with the best quality product. The same quality that is found in nature.<br />We further commit to inform and motivate user to live a healthy lifestyle by being in sync with nature. The biggest way to do that is our biggest physical input, which is our mouths. By serving the best health foods and health supplements, we wish to boost our customers&apos; health and lives.</p>
                             <h2>Our Vision</h2>
                             <p>At Puressence, we not only want our customers to live healthy lives, but to also learn how to firstly avoid those things that cripple our health.<br />Secondly, we hope to teach why health improves, so that true health in line with nature is not only a mere product, but a legacy that is taught down generations.
                             </p>
@@ -25,10 +25,10 @@ export default function AboutPage() {
                         </div>
                     </div>
                     <div className="subCont">
-                        <h2>What's Behind the Name?</h2>
-                        <p>In herbal medicine and in conventional medicine, each herb or medicine has an intrinsic value due to which it is used or applied. Likewise, each food, drink, herb and supplement has an intrinsic value. This value however can be reduced by time, elements, adulteration and interaction. To ensure this doesn't happen, we make sure to keep our products of the best and freshest quality, pure and securely packed.<br /> This is how the name PureEssence comes to be.</p><br />
+                        <h2>What&apos;s Behind the Name?</h2>
+                        <p>In herbal medicine and in conventional medicine, each herb or medicine has an intrinsic value due to which it is used or applied. Likewise, each food, drink, herb and supplement has an intrinsic value. This value however can be reduced by time, elements, adulteration and interaction. To ensure this doesn&apos;t happen, we make sure to keep our products of the best and freshest quality, pure and securely packed.<br /> This is how the name PureEssence comes to be.</p><br />
                         <h2>What PurEssence Ensures for You</h2>
-                        <p>Being pure doesn't always come easy. Especially in the fast modern world, it can be tempting to just get it over and done with. But... <br /><br />
+                        <p>Being pure doesn&apos;t always come easy. Especially in the fast modern world, it can be tempting to just get it over and done with. But... <br /><br />
                             <p className="aboutQuote" style={{ textAlign: 'center' }}>&quot; With Great Pureness, Comes Great Responsibility.&quot;</p><br />
                             To ensure we live up to this standard, we ensure to provide:
                         </p>
@@ -58,10 +58,10 @@ export default function AboutPage() {
                             <p>Hi there, I hope you are just of a natural lover as I am.<br /><br />
                             Sometimes though, nature is not always nice. There are ill days and rough days. But that is part of life.<br/><br />
                             Despite this I consider myself fortunate to have come from a family of herbal practitioners. My father Hakim Muhammad Naseer and my maternal grandfather Hakim Ismail Rabbani were both qualified Herbal practitioners. I grew up having to know how not only medicine, but food effects us. In fact, when our intake (food, water and whatever enters our stomachs) is good and in order, then for the most part our health is in check.<br /><br />
-                            In my life, I rarely have had to go to hospital. That is comparing to the average modern citizen. That doesn't mean I wouldn't get ill. Rather, for the most part I would be treated using remedies from nature.<br /><br />
+                            In my life, I rarely have had to go to hospital. That is comparing to the average modern citizen. That doesn&apos;t mean I wouldn&apos;t get ill. Rather, for the most part I would be treated using remedies from nature.<br /><br />
                             Today, their lives have passed, but their legacy lives on. Through the beliefs of <i><b>You Are What You Eat</b></i> and <i><b>There is Always a Cure in Nature,</b></i> I along with my family ensure we live our lives by eating healthy, and keep our health in top conditions using pure natural products.<br /><br />
                             Having known these widespread blessings in nature, and without relying too much on Big Pharmaceuticals (whilst still respecting and using conventional medicine), I want to share this with you. Science does not invent solutions. It only discovers it. They were there all along.<br /><br />
-                            Just like those who have gone before us, we too shall pass. It's best to live healthier and stronger, whilst doing good and spreading good. I hope you join me in the mission to do good. To be kind and caring to ourselves and others, by eating, drinking and healing in the best and most natural way possible, and encouraging others to do so too.<br /><br />
+                            Just like those who have gone before us, we too shall pass. It&apos;s best to live healthier and stronger, whilst doing good and spreading good. I hope you join me in the mission to do good. To be kind and caring to ourselves and others, by eating, drinking and healing in the best and most natural way possible, and encouraging others to do so too.<br /><br />
                             Last but not least, sharing is caring. The best way to preserve knowledge is by writing it down. That is why I will be sharing my journey, as well as sharing ideas, tips and advice on how to live a pure and healthy life.<br /><br />
                             <i><b>~ Hashaam Naseer</b></i>
                             </p>
